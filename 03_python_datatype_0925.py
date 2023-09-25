4385abcecaba769613c6a15fb84a646070bfdc7c
Hyejeong Cho <127905686+kopometaverse@users.noreply.github.com>
2023-09-25 13:11:28 +0900

    Add files via upload

---
+++ 03_python_datatype_0925.py
@@ -0,0 +1,11 @@
+#--- 파이썬 데이터 타입
+int(3.5) # 3 - 정수형
+2e3 # 2000.0 2*(10**3)
+float("1.6") # 1.6 - 소수점
+float("inf") # 무한대
+float("-inf") # -무한대
+bool(0) # False. 숫자에서 0만 False임,
+bool(-1) # True
+bool("False") # True
+a = None # a는 None
+a is None # a가 None 이므로 True
